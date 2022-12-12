2f4188864f2f45b9d3cf877fedb3adeb7cd48ddb
akalManNakarmi <akalnakarmi1@gmail.com>
2022-12-12 21:15:51 +0545

    c&i 2 eg

---
+++ Day 1/Examples/compiler2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter");
+    scanf("%d",&a);
+    printf("a is %d",a);
+    return 0;
+}
---
+++ Day 1/Examples/interpreter2.py
@@ -0,0 +1,3 @@
+
+a = input("Enter")
+# pritn(a)
